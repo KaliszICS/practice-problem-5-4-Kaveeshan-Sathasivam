923db33031d2a858435095f4eb3ad5955c4dcf39
Kaveeshan-Sathasivam <343279329@tdsb.ca>
2026-05-04 15:37:53 +0000

    derfghj

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,34 @@
+/*File: Practice Problem 5.4
+Author: Kaveeshan Sathasivam
+Date Created: May 4th, 2026
+Date Last Modified: May 4th, 2026
+*/
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		
 	}
+	public static void stringSwap(String[] arr, int i, int j) {
+   		String temp = arr[i];
+    	arr[i] = arr[j];
+    	arr[j] = temp;
+    }
+	public static void reverse(int[] arr) {
+        int temp;
+        
+        for (int i = 0; i < arr.length / 2; i++) {
+            temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+    }
+	public static String[] toWordArray(String str) {
+    str = str.trim();
 
+    if (str.equals("")) {
+        return new String[0]; // 
+    }
+
+    return str.split("\\s+");
+}
 }
